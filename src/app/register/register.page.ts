7ac00f8c2a850f0c3a2c354a1492d543ed6d805e
SaulACC <saul_chavira2000@hotmail.com>
2022-03-02 17:42:59 -0700

    Login and register

---
+++ src/app/register/register.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, Validator, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  formRegister: FormGroup;
+
+  constructor(public fb: FormBuilder) {
+    this.formRegister = this.fb.group({
+      name: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required),
+      email: new FormControl('',Validators.required)
+    });
+   }
+
+  ngOnInit() {
+  }
+}
